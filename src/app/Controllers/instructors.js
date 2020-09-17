04cb84cdd9dbdd0f4b74014bc621c727b9692018
Rwan <rwan.souza@hotmail.com>
2020-09-17 17:53:49 -0300

    Update & delete

--- src/app/Controllers/instructors.js
+++ src/app/Controllers/instructors.js
@@ -22,7 +22,7 @@ module.exports = {
       instructor.created_at = date(instructor.created_at).format;
 
       return res.render('instructors/show', { instructor});
-    })
+    });
   },
 
   post(req, res){
@@ -35,11 +35,16 @@ module.exports = {
 
     Instructor.create(req.body, function(instructor){
       return res.redirect(`/instructors/${instructor.id}` );
-    })
+    });
   },
 
   edit(req, res){
-    return
+    Instructor.find(req.params.id, function(instructor){
+
+      instructor.birth = date(instructor.birth).iso;
+    
+      return res.render('instructors/edit', { instructor});
+    });
   },
 
   put(req, res){
@@ -50,11 +55,15 @@ module.exports = {
         return res.send('Please, fill all fields!');
     }
 
-    return
+    Instructor.update(req.body, function(){
+      return res.redirect(`/instructors/${req.body.id}`)
+    });
   },
 
   delete(req, res){
-    return
+    Instructor.delete(req.body.id, function(){
+      return res.redirect(`/instructors/`)
+    });
   },
 }
 
