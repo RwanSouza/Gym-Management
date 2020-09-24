b6b6127a09947b1a68cecf6416e7faa6f34541c9
Rwan <rwan.souza@hotmail.com>
2020-09-24 18:37:25 -0300

    Pagination members

--- src/app/Controllers/members.js
+++ src/app/Controllers/members.js
@@ -3,10 +3,30 @@ const { date, age } = require('../lib/utils');
 
 module.exports = {
   index(req, res){
+    let { filter, page, limit } = req.query
 
-    Member.all(function(members){
-      return res.render('members/index', {members});
-    });
+    page = page || 1
+    limit = limit || 4
+    let offset = limit * (page - 1)
+
+    const params = {
+      filter,
+      page,
+      limit,
+      offset,
+      callback(members) {
+
+        const pagination = {
+          total:Math.ceil(members[0].total / limit),
+          page
+        }
+
+        return res.render('members/index', { members, filter, pagination });
+      }
+    }
+
+    Member.paginate(params)
+
   },
 
   create(req, res){
